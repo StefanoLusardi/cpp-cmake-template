c3ba0e466dae89854363467a8e7ea55f206407c2
Stefano Lusardi <stefano.lusardi@teiacare.com>
2022-04-07 14:48:15 +0200

    Added dll import/export for windows

--- core/include/core/core.hpp
+++ core/include/core/core.hpp
@@ -1,6 +1,23 @@
 #pragma once
 
-class core
+#pragma once
+
+#if defined(_WIN32) || defined(_WIN64)
+	#define API_EXPORT __declspec(dllexport)
+	#define API_IMPORT __declspec(dllimport)
+#else
+	#define API_EXPORT __attribute__ ((visibility ("default")))
+	#define API_IMPORT __attribute__ ((visibility ("default")))
+#endif
+
+#ifdef LIB_CORE
+	#define LIB_CORE API_EXPORT
+#else
+	#define LIB_CORE API_IMPORT
+#endif 
+
+
+class LIB_CORE core
 {
 public:
 	explicit core(int x);
